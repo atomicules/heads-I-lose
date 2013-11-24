18da45093abbe66eb412b70bc1f8371b9ba161d7
atomicules <base@atomicules.co.uk>
2013-11-24 22:29:46 +0000

    Get basic XPath working, searching for date and time

--- headsilose.erl
+++ headsilose.erl
@@ -1,6 +1,7 @@
 -module(headsilose).
 %-export([headsilose/2]).
--export([readapikey/0, get_locations/0]).
+-export([readapikey/0, get_locations/0, get_weather/1]).
+-include_lib("xmerl/include/xmerl.hrl").
 
 %Supply a direction and location and work out if head wind or not
 %Should be relatively easy
@@ -13,24 +14,37 @@
 -define(BASE_URL,
 	"http://datapoint.metoffice.gov.uk/public/data/").
 
--define(SITELIST,
-	   "val/wxfcs/all/datatype/sitelist").
+-define(WXFCS_SITELIST,
+	"val/wxfcs/all/xml/sitelist").
 
-datapoint_url(Location) ->
-	?BASE_URL ++ ?SITELIST ++ "?key=" ++ readapikey.
+-define(WXFCS_LOCATIONID,
+	"val/wxfcs/all/xml/").
 
 
 get_locations() ->
 	inets:start(),
 	Key = readapikey(),
-	URL = ?BASE_URL ++ ?SITELIST ++ "?key=" ++ Key,
+	URL = ?BASE_URL ++ ?WXFCS_SITELIST ++ "?key=" ++ Key,
 	{ ok, {_Status, _Headers, Body }} = httpc:request(URL),
 	Body.
 
 get_weather(Location) ->
-	URL = "g",
+	inets:start(),
+	Key = readapikey(),
+	URL = ?BASE_URL ++ ?WXFCS_LOCATIONID ++ Location ++ "?key=" ++ Key ++ "&res=3hourly",
 	{ ok, {_Status, _Headers, Body }} = httpc:request(URL),
-	Body.
+	{ Xml, _Rest } = xmerl_scan:string(Body),
+	[ #xmlAttribute{value=Direction} ]  = xmerl_xpath:string("//Period[@value='2013-11-28Z']/Rep[.='180']/@D", Xml),
+	Direction.
+
+%Todo:
+% Need to get current time and depnding on before/after 8am 5pm then analyse the evening/morning direction
+% Need somethng that understands compass directions
+%{{Year, Month, Day}, {Hours, _Minutes, _Seconds}} = erlang:localtime()
+%if ( Hours < 8 ) %then that day
+%if ( Hours < 19 ) ) %assume want home
+%if ( Hours > 19 ) ) %assume want next day weather
+% Change xpath filter basically
 
 %Need to read API key from file
 readapikey() ->
