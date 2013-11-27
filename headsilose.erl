ef8d8bac1e34de2b12c30b16a5ca1e49a8077db2
atomicules <base@atomicules.co.uk>
2013-11-27 11:32:16 +0000

    Basic functionality all working now!
    For a given location and heading (direction of travel) will tell me
    whether heads_i_lose (headwind) or tails_i_win (tailwind).
    
    Since wind direction is the from direction and heading (as far as I'm
    concerned) is the to direction, a head wind is defined as anything 2
    compass points either side (on a 16-point compass). A tailwind is
    anything else - which isn't true, but I didn't say I was finished yet.
    
    Needs lots of tarting up as well.

--- headsilose.erl
+++ headsilose.erl
@@ -1,12 +1,9 @@
 -module(headsilose).
-%-export([headsilose/2]).
--export([readapikey/0, get_locations/0, get_weather/1]).
+-export([readapikey/0, get_locations/0, get_weather/1, direction_is_opposite/2, headsilose/2]).
 -include_lib("xmerl/include/xmerl.hrl").
 
 %Supply a direction and location and work out if head wind or not
 %For now "know the location id" upfront, but ideally need to search for it at some point or present a choice.
-
-%headsilose(Direction, Location) -> 
 
 %Based on: http://pragdave.pragprog.com/pragdave/2007/04/a_first_erlang_.html 
 
@@ -26,6 +23,16 @@ get_locations() ->
 	URL = ?BASE_URL ++ ?WXFCS_SITELIST ++ "?key=" ++ Key,
 	{ ok, {_Status, _Headers, Body }} = httpc:request(URL),
 	Body.
+
+
+headsilose(Location, Heading) ->
+	Wind = get_weather(Location),
+	Direction_is_opposite = direction_is_opposite(Heading, Wind),
+	if Direction_is_opposite ->
+		heads_you_lose;
+	true -> 
+		tails_you_win
+	end.
 
 
 get_weather(Location) ->
@@ -63,11 +70,35 @@ format_date(Date_to_format) ->
 	Thing.
 
 
+nth_wrap(N, List) ->
+	Rem = N rem (length(List)),
+	if Rem > 0 ->
+	   lists:nth(Rem, List);
+	Rem =:= 0 ->
+		1
+	end.
+
+
 find_next_day(Date_today) ->
 	%get in seconds
 	Seconds_today = calendar:datetime_to_gregorian_seconds(Date_today),
 	Date_tomorrow = calendar:gregorian_seconds_to_datetime(Seconds_today+86400),
 	Date_tomorrow.
+
+
+direction_is_opposite(Heading, Wind) ->
+	Compass = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"],
+	%Quicker dirtier(?) way to do below would be: http://stackoverflow.com/a/6762191/208793
+	Index = length(lists:takewhile(fun(X) -> X  =/= Wind end, Compass))+1,
+	%Since heading is to direction and winds are from, opposite is -2 to +2
+	%Or to make it easier to wrap, +14 +18
+	Opposites = [nth_wrap(Index+14, Compass),
+	             nth_wrap(Index+15, Compass),
+	             nth_wrap(Index+16, Compass),
+	             nth_wrap(Index+17, Compass),
+	             nth_wrap(Index+18, Compass)],
+	%Yuck! Must be better way to build that
+	lists:member(Heading, Opposites).
 
 
 %Need to read API key from file
