8bab193a5337c6a721554c4cd5bb9ddca1640b0d
atomicules <base@atomicules.co.uk>
2014-11-29 17:52:12 +0000

    Yay! Working prototype. Will sum up the distances of headwinds...
    ...for a polyline route.
    
    I'd made silly mistakes like forgetting which order the list of tuples
    were in. I.e {Distances, Wind} or {Wind, Distances}. It doesn't really
    matter which order I use, but I should back sure it is consistent and
    matches the variable naming.
    
    Now need to extend this to sum up all wind directions and check for
    other bugs (such as sum of distances making sense, headwinds are
    actually headwinds and not tailwinds, etc).

--- headsilose.erl
+++ headsilose.erl
@@ -205,18 +205,17 @@ headsilose(Location) ->
 	Polyline_decoded = polyline:decode(Polyline),
 	List_of_distances_and_compass = convert_lats_longs_to_distance_heading(Polyline_decoded),
 	%If now have a set of co-ords need to figure out distances and directions
-	List_of_distances_and_wind = lists:map(fun({Compass, Distance}) ->
+	List_of_distances_and_wind = lists:map(fun({Distance, Compass}) ->
 			Wind = head_side_or_tail_wind(Compass, [Headwinds, Sidewinds, Tailwinds]),
-			{Wind, Distance}
+			{Distance, Wind}
 		end,
 		List_of_distances_and_compass),
 	%lists:map to get distnaces only? Could also map each of the winds?
 	%Figure out for headwinds only for now
-	Headw = lists:filter(fun({Element, _}) ->
-		Element == headwind end,
+	Headw = lists:filter(fun({_Distance, Wind_type}) ->
+		Wind_type == headwind end,
 		List_of_distances_and_wind),
-	Sum_of_Headw = lists:foldl(fun({_wind, Distance}, Sum) -> Distance + Sum end, 0, Headw),
-	io:format(Sum_of_Headw).
+	Sum_of_Headw = lists:foldl(fun({Distance2, _Wind}, Sum) -> Distance2 + Sum end, 0, Headw).
 	%io:format("Direction: ~s~nSpeed: ~s mph~nGust: ~s mph~nWeather type: ~s~nTemperature: ~s deg C~n", [Direction, Speed, Gust, Weather_type, Temperature]).
 
 
